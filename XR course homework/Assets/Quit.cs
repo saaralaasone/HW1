44bac7b98e2c7219d50b62ebaf72f93865f181b0
saaralaasone <71126251+saaralaasone@users.noreply.github.com>
2026-01-19 17:47:19 +0200

    Update Quit.cs
    Added the #if, #else , #endif to code

--- XR course homework/Assets/Quit.cs
+++ XR course homework/Assets/Quit.cs
@@ -9,6 +9,11 @@ public class Quit : MonoBehaviour
         action.action.Enable();
         action.action.performed += (ctx) =>
         {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
         };
         
     }
