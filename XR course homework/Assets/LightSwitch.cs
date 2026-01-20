248916264aa8a0a9e44496a24540acfb306c13df
saaralaasone <salaason20@student.oulu.fi>
2026-01-20 12:03:55 +0200

    Update

--- XR course homework/Assets/LightSwitch.cs
+++ XR course homework/Assets/LightSwitch.cs
@@ -1,30 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class LightSwitch : MonoBehaviour
 {
-    public Light light;
+    public Light lights;
     public InputActionReference action;
     void Start()
     {
-        light = GetComponent<Light>();
-        action.action.Enable();
-        action.action.performed += OnLightToggle();
+        lights = GetComponent<Light>();
+        //action.action.Enable();
+        //action.action.performed += OnLightToggle();
     }
 
-    private void OnLightToggle(InputAction.CallbackContext ctx)
+    private void Update()
     {
-        if (light.color == Color.white)
-            light.color = Color.red;
-        else
-            light.color = Color.white;
-    }
-    
-    void OnDestroy()
-    {
-        action.action.performed -= OnLightToggle;
+        if (Input.GetKeyDown("tab"))
+        {
+            if (lights.color == Color.white)
+                lights.color = Color.red;
+            else
+                lights.color = Color.white;
+        }
     }
 }
