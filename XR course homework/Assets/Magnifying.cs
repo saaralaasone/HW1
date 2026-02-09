7edd2e62b7199a8c4aec8590d975bed3fe05353c
saaralaasone <71126251+saaralaasone@users.noreply.github.com>
2026-02-09 17:33:45 +0200

    I AM DONE
    AAAAAAAAAAAAAAAAAAAAA
    Still does not work...

--- XR course homework/Assets/Magnifying.cs
+++ XR course homework/Assets/Magnifying.cs
@@ -8,14 +8,9 @@ public class Magnifying : MonoBehaviour
     public Transform lens;
     void LateUpdate()
     {
-        //Vector3 dir = (lens.position - mainCamera.position);
-        //Vector3 up = lens.up;
-        //transform.rotation = Quaternion.LookRotation(dir, up);
-        //transform.position = lens.position - dir * 0.01f;
         transform.position = lens.position;
-        Vector3 dir = (lens.position - mainCamera.position);
-        //Vector3 forwardPoint = lens.position + (lens.position - mainCamera.position);
+        Vector3 dir = lens.position - mainCamera.position;
         Vector3 target = lens.position + dir;
-        transform.LookAt(target, lens.up);
+        transform.LookAt(target, Vector3.up); //change to vector3.up or lens.up
     }
 }
